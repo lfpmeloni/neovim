ad3727c98c9f3720ae26d130e383df85affdd34e
Felipe Meloni <felipemeloni@hotmail.com>
2025-02-24 10:47:14 +0100

    Initial commit: Add Neovim configurations

---
+++ lua/vim-options.lua
@@ -0,0 +1,15 @@
+vim.opt.shell = '"C:\\Program Files\\PowerShell\\7\\pwsh.exe"'
+vim.opt.shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command"
+vim.opt.shellredir = ">"
+vim.opt.shellpipe = "2>&1 | Tee-Object"
+vim.opt.shellquote = ""
+vim.opt.shellxquote = ""
+
+vim.cmd("set tabstop=4")
+vim.cmd("set shiftwidth=4")
+vim.g.mapleader = " "
+
+
+
+
+
